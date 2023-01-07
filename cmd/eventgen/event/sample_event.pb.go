5155353f4743761f3e8804c6957c42b72d0beaef
TAKA <twakiyama.biz@gmail.com>
2023-01-07 11:37:36 +0900

    fix: server codeを生成

--- cmd/eventgen/event/sample_event.pb.go
+++ cmd/eventgen/event/sample_event.pb.go
@@ -4,11 +4,77 @@
 
 package eventgen
 
-import "context"
+import (
+	"fmt"
+	"context"
+	"encoding/json"
+	"cloud.google.com/go/pubsub"
+)
 
 type HelloWorldService interface {
-	HelloWorld(ctx context.Context, req *HelloWorldRequest) EmptyResponse
-	// helloworldtopichelloworldsubscription
-	HogeEvent(ctx context.Context, req *HogeEventRequest) EmptyResponse
-	// hogehoge
+	HelloWorld(ctx context.Context, req *HelloWorldRequest) error
+	HogeEvent(ctx context.Context, req *HogeEventRequest) error
+}
+
+func Run(service HelloWorldService, client *pubsub.Client) {
+	ctx := context.Background()
+	if err := listenHelloWorld(ctx, service, client); err != nil {
+		panic(err)
+	}
+	if err := listenHogeEvent(ctx, service, client); err != nil {
+		panic(err)
+	}
+}
+func listenHelloWorld(ctx context.Context, service HelloWorldService, client *pubsub.Client) error {
+	subscriptionName := "helloworldsubscription"
+	topicName := "helloworldtopic"
+	// TODO: メッセージの処理時間の延長を実装する必要がある
+	callback := func(ctx context.Context, msg *pubsub.Message) {
+		msg.Ack()
+
+		var event HelloWorldRequest
+		if err := json.Unmarshal(msg.Data, &event); err != nil {
+			fmt.Println(err)
+		}
+		if err := service.HelloWorld(ctx, &event); err != nil {
+			msg.Nack()
+		}
+	}
+	err := pullMsgs(ctx, client, subscriptionName, topicName, callback)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func listenHogeEvent(ctx context.Context, service HelloWorldService, client *pubsub.Client) error {
+	subscriptionName := "hoge"
+	topicName := "hoge"
+	// TODO: メッセージの処理時間の延長を実装する必要がある
+	callback := func(ctx context.Context, msg *pubsub.Message) {
+		msg.Ack()
+
+		var event HogeEventRequest
+		if err := json.Unmarshal(msg.Data, &event); err != nil {
+			fmt.Println(err)
+		}
+		if err := service.HogeEvent(ctx, &event); err != nil {
+			msg.Nack()
+		}
+	}
+	err := pullMsgs(ctx, client, subscriptionName, topicName, callback)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func pullMsgs(ctx context.Context, client *pubsub.Client, subScriptionName, topicName string, callback func(context.Context, *pubsub.Message)) error {
+	sub := client.Subscription(subScriptionName)
+	// topic := client.Topic(topicName)
+	fmt.Printf("topicName: %v\n", topicName)
+	err := sub.Receive(ctx, callback)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- cmd/protoc-gen-go-event/main.go
+++ cmd/protoc-gen-go-event/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"flag"
+	"errors"
 
 	"github.com/TakaWakiyama/forcusing-backend/cmd/protoc-gen-go-event/option"
-
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/descriptorpb"
@@ -15,15 +14,22 @@ import (
 // https://github.com/golang/protobuf/issues/1260
 // protoc --go_out=. --go_opt=paths=source_relative  --go-event_out=. --go-event_opt=paths=source_relative sample.proto
 
-var font *string
+const (
+	GO_IMPORT_FMT     = "fmt"
+	GO_IMPORT_CONTEXT = "context"
+	GO_IMPORT_JSON    = "encoding/json"
+	GO_IMPORT_PUBSUB  = "cloud.google.com/go/pubsub"
+)
+
+var allImportMods = []string{
+	GO_IMPORT_FMT,
+	GO_IMPORT_CONTEXT,
+	GO_IMPORT_JSON,
+	GO_IMPORT_PUBSUB,
+}
 
 func main() {
-	var flags flag.FlagSet
-	font = flags.String("font", "doom", "font list available in github.com/common-nighthawk/go-figure")
-
-	protogen.Options{
-		ParamFunc: flags.Set,
-	}.Run(func(gen *protogen.Plugin) error {
+	protogen.Options{}.Run(func(gen *protogen.Plugin) error {
 		for _, f := range gen.Files {
 			if !f.Generate {
 				continue
@@ -46,7 +52,11 @@ func generateFile(gen *protogen.Plugin, file *protogen.File) *protogen.Generated
 	g.P()
 	g.P("package ", file.GoPackageName)
 	g.P()
-	g.P(`import "context"`)
+	g.P("import (")
+	for _, mod := range allImportMods {
+		g.P(`"`, mod, `"`)
+	}
+	g.P(")")
 
 	if len(file.Services) == 0 {
 		return g
@@ -54,19 +64,78 @@ func generateFile(gen *protogen.Plugin, file *protogen.File) *protogen.Generated
 	svc := file.Services[0]
 
 	svcName := svc.GoName
+	// Create Client Code
+
+	// Create Server Code
+	// Create interface
 	g.P("type ", svcName, " interface {")
 	for _, m := range svc.Methods {
 		// g.P("// ", m.Comments.Leading, " ", m.Comments.Trailing)
-		g.P(m.GoName, "(ctx context.Context, req *", m.Input.GoIdent, ") ", m.Output.GoIdent)
-		options := m.Desc.Options().(*descriptorpb.MethodOptions)
-		ext := proto.GetExtension(options, option.E_PubSubOption)
-		opt, ok := ext.(*option.PubSubOption)
-		if !ok {
-			continue
-		}
-		g.P("//", opt.Topic, opt.Subscription)
+		g.P(m.GoName, "(ctx context.Context, req *", m.Input.GoIdent, ") error ")
 	}
 	g.P("}")
 
+	// Create Run Function
+	g.P("func Run(service ", svcName, ", client *pubsub.Client) {")
+	g.P("ctx := context.Background()")
+	for _, m := range svc.Methods {
+		g.P("if err := listen", m.GoName, "(ctx, service, client); err != nil {")
+		g.P("panic(err)")
+		g.P("}")
+	}
+	g.P("}")
+	// Create listen function
+	for _, m := range svc.Methods {
+		opt, _ := getPubSubOption(m)
+		g.P("func listen", m.GoName, "(ctx context.Context, service ", svcName, ", client *pubsub.Client) error {")
+		g.P("subscriptionName := ", `"`, opt.Subscription, `"`)
+		g.P("topicName := ", `"`, opt.Topic, `"`)
+		g.P("//", "TODO: メッセージの処理時間の延長を実装する必要がある")
+		// https://christina04.hatenablog.com/entry/cloud-pubsub
+		g.P(`callback := func(ctx context.Context, msg *pubsub.Message) {
+			msg.Ack()
+		`)
+		g.P("var event ", m.Input.GoIdent)
+		g.P("if err := json.Unmarshal(msg.Data, &event); err != nil {")
+		g.P("fmt.Println(err)")
+		// error 処理
+		g.P("}")
+		g.P("if err := service.", m.GoName, "(ctx, &event); err != nil {")
+		// 再送信させる
+		g.P("msg.Nack()")
+		g.P("}")
+		g.P("}")
+		g.P("err := pullMsgs(ctx, client, subscriptionName, topicName, callback)")
+		g.P("if err != nil {")
+		g.P("return err")
+		g.P("}")
+		g.P("return nil")
+		g.P("}")
+	}
+
+	// Create pullMsgs function
+	g.P(`
+	func pullMsgs(ctx context.Context, client *pubsub.Client, subScriptionName, topicName string, callback func(context.Context, *pubsub.Message)) error {
+		sub := client.Subscription(subScriptionName)
+		// topic := client.Topic(topicName)
+		fmt.Printf("topicName: %v\n", topicName)
+		err := sub.Receive(ctx, callback)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	`)
+
 	return g
+}
+
+func getPubSubOption(m *protogen.Method) (*option.PubSubOption, error) {
+	options := m.Desc.Options().(*descriptorpb.MethodOptions)
+	ext := proto.GetExtension(options, option.E_PubSubOption)
+	opt, ok := ext.(*option.PubSubOption)
+	if !ok {
+		return nil, errors.New("no pubsub option")
+	}
+	return opt, nil
 }
